5a05e8c259bffb526f5abee1de30897ff8ebe170
chaython <chaython@live.ca>
2026-04-13 07:15:02 -0600

    UTF8-encoding issues

--- WMT-GUI.ps1
+++ WMT-GUI.ps1
@@ -9,7 +9,7 @@
 # ==========================================
 # 1. SETUP
 # ==========================================
-$AppVersion = "5.2"
+$AppVersion = "5.3"
 $ErrorActionPreference = "SilentlyContinue"
 [Console]::OutputEncoding = [System.Text.UTF8Encoding]::new($false)
 $OutputEncoding = [System.Text.UTF8Encoding]::new($false)
